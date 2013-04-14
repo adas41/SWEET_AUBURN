3a18208f267d99cebd1e45405a91e76a1609b1d1
jamesgk <jamesgk19@gmail.com>
2013-04-13 21:03:29 -0400

    Polishing initial interface.

--- ar.js
+++ ar.js
@@ -1,3 +1,5 @@
+var bottomBarTimeoutId = -1;
+
 function startTour(){
 	if(typeof(Storage)!=="undefined"){
 		localStorage.name = 'Butler Street YMCA';
@@ -13,52 +15,48 @@ function startTour(){
 function openMenuScreen(locationName) {
     closeMenuScreen();
     closeMarkerScreen();
-    popupBox(locationName, "10%", "15%", "lightgray");
-    popupBox("Take me there!", "10%", "40%", "yellow", function() {
+    popupBox(locationName, "48px", "15%");
+    popupBox("Take me there!", "48px", "40%", function() {
 		startTour();
     });
-    popupBox("Virtual Tour", "10%", "50%", "yellow", function() {
+    popupBox("Virtual Tour", "48px", "50%", function() {
         closeMenuScreen();
         openPanorama();
     });
-    popupBox("Cancel", "10%", "60%", "yellow", function() {
+    popupBox("Cancel", "48px", "60%", function() {
         closeMenuScreen();
         openMarkerScreen();
     });
 }
 
-function popupBox(label, height, topOffset, bgColor, onclickEvent) {
+function popupBox(label, height, topOffset, onclickEvent) {
     var popupDiv = document.createElement("div");
-    //var textDiv = document.createElement("div");
     popupDiv.className = "menuScreen";
     
-    popupDiv.style.color = "black";
     popupDiv.style.fontFamily = "sans-serif";
     popupDiv.style.fontSize = "24px";
-    popupDiv.style.lineHeight = "48px";
-    //textDiv.style.position = "absolute";
-    //textDiv.style.top = "12px";
-    //textDiv.style.width = "100%";
+    popupDiv.style.lineHeight = height;
     popupDiv.innerText = label;
     
-    if (bgColor) {
-        popupDiv.style.backgroundColor = bgColor;
-    } else {
-        popupDiv.style.backgroundColor = "yellow";
-    }
-    popupDiv.style.border = "1px solid gray";
+    popupDiv.style.border = "2px solid white";
     popupDiv.style.height = height;
-    popupDiv.style.left = "64px";
-    popupDiv.style.opacity = "0.6";
+    popupDiv.style.overflow = "scroll";
     popupDiv.style.position = "absolute";
     popupDiv.style.top = topOffset;
-    popupDiv.style.width = "192px";
     
     if (onclickEvent) {
+        popupDiv.style.backgroundColor = "rgba(255, 255, 0, 0.6)";
+        popupDiv.style.color = "black";
+        popupDiv.style.left = "64px";
+        popupDiv.style.width = "192px";
         popupDiv.addEventListener("click", onclickEvent);
+    } else {
+        popupDiv.style.backgroundColor = "rgba(43, 39, 39, 0.6)";
+        popupDiv.style.color = "white";
+        popupDiv.style.left = "48px";
+        popupDiv.style.width = "224px";
     }
     
-    //popupDiv.appendChild(textDiv);
     document.body.appendChild(popupDiv);
 }
 
@@ -68,23 +66,27 @@ function bottomBar(label) {
     var bufferDiv = document.createElement("div");
     var handleDiv = document.createElement("div");
     var textDiv = document.createElement("div");
+    var barSize = 64, handleSize = 14, gradientSize = 16;
     
+    gradientDiv.className = "markerScreen";
     gradientDiv.style.backgroundImage = "url(\"bar.png\")";
-    gradientDiv.style.height = "16px";
+    gradientDiv.style.height = gradientSize + "px";
     gradientDiv.style.left = "0";
     gradientDiv.style.position = "absolute";
-    gradientDiv.style.top = "464px";
+    gradientDiv.style.top = (480 - gradientSize) + "px";
     gradientDiv.style.width = "320px";
     
     wrapperDiv.className = "markerScreen";
-    wrapperDiv.style.height = "68px";
+    wrapperDiv.id = "bottomBar";
+    wrapperDiv.style.height = barSize + "px";
     wrapperDiv.style.left = "0";
     wrapperDiv.style.overflow = "scroll";
     wrapperDiv.style.position = "absolute";
-    wrapperDiv.style.top = "428px";
+    wrapperDiv.style.top = (480 - barSize) + "px";
     wrapperDiv.style.width = "320px";
+    wrapperDiv.addEventListener("scroll", setBottomBarCloseTimer);
     
-    bufferDiv.style.height = "68px";
+    bufferDiv.style.height = barSize + "px";
     bufferDiv.style.left = "0";
     bufferDiv.style.position = "absolute";
     bufferDiv.style.top = "0";
@@ -92,25 +94,26 @@ function bottomBar(label) {
     wrapperDiv.appendChild(bufferDiv);
     
     handleDiv.style.backgroundColor = "gray";
-    handleDiv.style.height = "16px";
+    handleDiv.style.height = handleSize + "px";
     handleDiv.style.left = "0";
     handleDiv.style.opacity = "0.6";
     handleDiv.style.position = "absolute";
-    handleDiv.style.top = "68px";
+    handleDiv.style.top = barSize + "px";
     handleDiv.style.width = "320px";
     wrapperDiv.appendChild(handleDiv);
     
     textDiv.style.backgroundColor = "yellow";
     textDiv.style.color = "black";
     textDiv.style.fontFamily = "sans-serif";
-    textDiv.style.fontSize = "18px";
-    textDiv.style.height = "52px";
+    textDiv.style.fontSize = "20px";
+    textDiv.style.height = (barSize - handleSize) + "px";
     textDiv.style.left = "0";
+    textDiv.style.lineHeight = (barSize - handleSize) + "px";
     textDiv.style.opacity = "0.6";
     textDiv.style.position = "absolute";
-    textDiv.style.top = "84px";
+    textDiv.style.top = (barSize + handleSize) + "px";
     textDiv.style.width = "320px";
-    textDiv.innerText = "More locations...";
+    textDiv.innerText = "More locations on Auburn Ave...";
     textDiv.addEventListener("click", function() {
         closeMarkerScreen();
         openLocationList();
@@ -120,23 +123,12 @@ function bottomBar(label) {
     document.body.appendChild(gradientDiv);
     document.body.appendChild(wrapperDiv);
 }
-function openBottomBar() {
-    var curDiv = document.getElementById("bottomBarText");
-    curDiv.style.display = "block";
-    curDiv.className = "markerScreen";
-    curDiv = document.getElementById("bottomBarHandle");
-    curDiv.style.top = "356px";
-    curDiv.removeEventListener("click", openBottomBar);
-    curDiv.addEventListener("click", closeBottomBar);
+function hideBottomBar() {
+    $("#bottomBar").animate({ scrollTop: 0 }, 500);
 }
-function closeBottomBar() {
-    var curDiv = document.getElementById("bottomBarText");
-    curDiv.style.display = "none";
-    curDiv.className = "";
-    curDiv = document.getElementById("bottomBarHandle");
-    curDiv.style.top = "394px";
-    curDiv.removeEventListener("click", closeBottomBar);
-    curDiv.addEventListener("click", openBottomBar);
+function setBottomBarCloseTimer() {
+    window.clearTimeout(bottomBarTimeoutId);
+    bottomBarTimeoutId = window.setTimeout(hideBottomBar, 1500);
 }
 
 function openMarkerScreen() {
@@ -144,6 +136,7 @@ function openMarkerScreen() {
     for (var i = 0; i < markerPopups.length; i++) {
         markerPopups.item(i).style.display = "block";
     }
+    setBottomBarCloseTimer();
 }
 
 function closeMarkerScreen() {
@@ -165,37 +158,48 @@ function openLocationList() {
     var listDiv = document.createElement("div");
     listDiv.className = "menuScreen";
     
-    listDiv.style.height = "40%";
-    listDiv.style.left = "48px";
+    listDiv.style.border = "1px solid white";
+    listDiv.style.height = "65%";
+    listDiv.style.left = "24px";
     listDiv.style.overflow = "scroll";
     listDiv.style.position = "absolute";
-    listDiv.style.top = "15%";
-    listDiv.style.width = "224px";
+    listDiv.style.top = "10%";
+    listDiv.style.width = "272px";
     
-    var myPlaces = ["Atlanta Daily World", "Butler Street YMCA", 
-        "Herndon Building", "Wheat Street Baptist Church"];
+    var myPlaces = ["Atlanta Daily World", "Atlanta Life Insurance", 
+        "Big Bethal A.M.E. Church", "Butler Street YMCA", "Herndon Building", 
+        "Odd Fellows Building", "Prince Hall Masonic Temple", "Tabor Building", 
+        "Wheat Street Baptist Church"];
     for (var i = 0; i < myPlaces.length; i++) {
         var buttonDiv = document.createElement("div");
+        buttonDiv.id = "listItem" + i;
         buttonDiv.style.backgroundColor = "yellow";
-        buttonDiv.style.border = "1px solid gray";
+        buttonDiv.style.border = "1px solid white";
         buttonDiv.style.color = "black";
         buttonDiv.style.fontFamily = "sans-serif";
-        buttonDiv.style.fontSize = "18px";
-        buttonDiv.style.height = "30%";
+        buttonDiv.style.fontSize = "19px";
+        buttonDiv.style.height = "48px";
+        buttonDiv.style.lineHeight = "48px";
         buttonDiv.style.opacity = "0.6";
+        buttonDiv.style.paddingLeft = "8px";
         buttonDiv.style.textAlign = "left";
-        buttonDiv.style.top = (30 * i) + "%";
+        buttonDiv.style.top = (48 * i) + "px";
         buttonDiv.innerText = myPlaces[i];
-        buttonDiv.addEventListener("click", function() {
-            openMenuScreen(myPlaces[i]);
-        });
         
         listDiv.appendChild(buttonDiv);
     }
     
     document.body.appendChild(listDiv);
+    for (var i = 0; i < myPlaces.length; i++) {
+        var curItem = $("#listItem" + i);
+        curItem.on("click", { locationName: curItem.prop("innerText") }, 
+            function(event) {
+                openMenuScreen(event.data.locationName);
+            }
+        );
+    }
     
-    popupBox("Cancel", "10%", "60%", "yellow", function() {
+    popupBox("Cancel", "48px", "80%", function() {
         closeMenuScreen();
         openMarkerScreen();
     });
@@ -211,7 +215,7 @@ function openPanorama() {
     
     geoSpot = new AR.GeoSpotPanorama( urls );
     
-    popupBox("Close panorama", "10%", "80%", "yellow", function() {
+    popupBox("Close panorama", "48px", "80%", function() {
         closeMenuScreen();
         geoSpot.hide();
         openMarkerScreen();
